4f02d41e6786c86eed6e2ea57b5140a9e0da7c01
nkrell <98400970+nkrell@users.noreply.github.com>
2022-09-17 15:28:38 -0400

    Update Chr_parser_II.py

--- Chr_parser_II.py
+++ Chr_parser_II.py
@@ -19,6 +19,45 @@ def main():
 	#navigate into target folder
 	os.chdir(targetFolder)
 
+	#read in each genome
+	for file in os.listdir():
+		if file.endswith(".fna"):
+			readInGenome(file)
+
+def readInGenome(filename):
+	#declare required temporary variables
+	chrLength = 0
+	chrName = ""
+	#create Genome object
+	currentGenome = Genome(filename)
+	#open file
+	with open(filename) as fh:
+		for line in fh:
+			if line.startswith(">"):
+						#trim header to assention number
+						line = line.split(" ")
+						line = line[0]
+						chrName = line
+						if chrLength != 0:
+							#lengthList.append(chrLength)
+							#create chromosome object for parsed chromosome data
+							currentChromosome = Chromosome(chrName, 1, chrLength, 0)
+							#add chromosome object to currentGenome
+							currentGenome.addChromosome(currentChromosome)
+							chrLength = 0
+			else:
+				chrLength += len(line.strip("\n"))
+		#add last chromosome as there are no more ">"
+		#lengthList.append(chrLength)
+		#create chromosome object for parsed chromosome data
+		currentChromosome = Chromosome(chrName, 1, chrLength, 0)
+		#add chromosome object to currentGenome
+		currentGenome.addChromosome(currentChromosome)
+
+	#testing
+	print(currentGenome)
+	
+
 
 
 
@@ -26,11 +65,45 @@ def main():
 ###############################CLASSES#################################
 class Genome:
 	def __init__(self, genomeName):
-		#initilize only the name witha value, the list will be filled via method later
+		#initilize only the name with a value, the list will be filled via method later
 		self.genomeName = genomeName
 		self.chrList = list()
 
-	def fillChrList()
+	def addChromosome(self, chromosome):
+		#add new chromosome to list of chromosomes
+		self.chrList.append(chromosome)
+
+	def __str__(self):
+		print(self.genomeName)
+		for chromosome in self.chrList:
+			print(chromosome)
+		return(str(len(self.chrList)))
+
+class Chromosome:
+
+	def __init__(self, name, start, end, cent):
+		self.name = name
+		self.start = start
+		self.end = end	
+		self.cent = cent
+
+	def addCent(self, newCent):
+		self.cent = newCent
+
+	def __str__(self):
+		return(self.name + "\t" + str(self.start) + "\t" + str(self.end) + "\n")
+
+	def chrName(self):
+		return(self.name)
+
+	def start(self):
+		return(self.start)
+
+	def end(self):
+		return(self.end)
+
+	def writeLine(self):
+		return(self.name + "\t" + str(self.start) + "\t" + str(self.end) + "\n")
 
 
 if __name__ == '__main__':
